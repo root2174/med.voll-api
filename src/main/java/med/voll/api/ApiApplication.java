bcf1a4ae8cd3bb66aa801807c9567601ff208f1b
root2174 <lm031719@gmail.com>
2022-12-28 21:11:33 -0300

    medico post controller

--- src/main/java/med/voll/api/ApiApplication.java
+++ src/main/java/med/voll/api/ApiApplication.java
@@ -2,11 +2,25 @@ package med.voll.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ApiApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ApiApplication.class, args);
+  }
 
+  @Bean
+  public WebMvcConfigurer corsMappingConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("PUT", "DELETE, POST, GET");
+      }
+    };
+  }
 }
