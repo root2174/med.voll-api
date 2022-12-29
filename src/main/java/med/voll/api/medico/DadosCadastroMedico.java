f95339713dcc38ceec9b47dd33d0d363d2078d9b
root2174 <lm031719@gmail.com>
2022-12-29 19:15:36 -0300

    adicionando novo campo telefone na tabela medico

--- src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -10,6 +10,7 @@ import med.voll.api.endereco.DadosEndereco;
 public record DadosCadastroMedico(
     @NotBlank String nome,
     @NotBlank @Email String email,
+    @NotBlank String telefone,
     @NotBlank @Pattern(regexp = "\\d{4,6}") String crm,
     @NotNull Especialidade especialidade,
     @NotNull @Valid DadosEndereco endereco) {
