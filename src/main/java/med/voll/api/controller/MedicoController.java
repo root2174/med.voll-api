795bd24f9a200be5a50d66b77a4d0420bfd62adc
root2174 <lm031719@gmail.com>
2022-12-30 14:15:11 -0300

    Atualizacao de medico por ID

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,12 +1,10 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.medico.DadosCadastroMedico;
-import med.voll.api.medico.DadosListagemMedico;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
+import med.voll.api.medico.*;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,13 +25,22 @@ public class MedicoController {
     repository.save(new Medico(dados));
   }
 
-//  http://localhost:8080/medicos?size=1&page=0 -- Paginacao
+//  http://localhost:8080/medicos?size=2&page=0&sort=nome,desc -- Paginacao
   @GetMapping
   public Page<DadosListagemMedico> listar(
-          Pageable paginacao
+          @PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao
   ) {
     return repository
             .findAll(paginacao)
             .map(DadosListagemMedico::new);
+  }
+
+  @PutMapping
+  @Transactional
+  public void atualizar(
+          @RequestBody @Valid DadosAtualizacaoMedico dados
+  ) {
+    Medico medico = repository.getReferenceById(dados.id());
+    medico.atualizarInformacoes(dados);
   }
 }
--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -37,4 +37,18 @@ public class Medico {
     this.endereco = new Endereco(dados.endereco());
     this.especialidade = dados.especialidade();
   }
+
+  public void atualizarInformacoes(DadosAtualizacaoMedico dados) {
+    if (dados.nome() != null) {
+      this.nome = dados.nome();
+    }
+
+    if (dados.telefone() != null) {
+      this.telefone = dados.telefone();
+    }
+
+    if(dados.endereco() != null) {
+      this.endereco.atualizarInformacoes(dados.endereco());
+    }
+  }
 }
